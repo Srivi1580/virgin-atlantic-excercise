ae21a277c87d14abe974e50244774fad02668712
srivi1580 <dsrivasavi1580@gmail.com>
2024-02-19 14:55:09 +0000

    virgin atlantic created

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+import { SearchContextProvider } from "./store/search-context";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <SearchContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </SearchContextProvider>
+  </React.StrictMode>,
+  rootElement
+);
